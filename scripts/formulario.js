6309ac48643c5bd056e2796404cc6c1bdb273855
belfortep <pbelforte2003@gmail.com>
2023-01-24 15:03:02 -0300

    añadido uso del localStorage

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -3,14 +3,24 @@ let nombre = document.getElementById('name')
 let mail = document.getElementById('email')
 let mensaje = document.getElementById('message')
 
-let obtenerDatos = document.getElementById('obtener-datos')
 let datosMensaje = document.getElementById('datos-mensaje')
 let mensajes = document.getElementsByClassName('mensaje-form')
+let borrarDatos = document.getElementById('borrar-datos')
 
 datosMensaje.style.visibility = 'hidden'
+borrarDatos.style.visibility = 'hidden'
 
-if (!localStorage.getItem('nombre') || !localStorage.getItem('mail') || !localStorage.getItem('mensaje')) {
-        obtenerDatos.style.visibility = 'hidden'
+function actualizarDatosMensaje() 
+{
+        mensajes[0].innerHTML = "Nombre: " + localStorage.getItem('nombre')
+        mensajes[1].innerHTML = "Mail: " + localStorage.getItem('mail')
+        mensajes[2].innerHTML = "Mensaje: " + localStorage.getItem('mensaje')
+}
+
+if (localStorage.getItem('nombre') && localStorage.getItem('mail') && localStorage.getItem('mensaje')) {
+        actualizarDatosMensaje()
+        datosMensaje.style.visibility = 'visible'
+        borrarDatos.style.visibility = 'visible'
 }
 
 formulario.addEventListener('submit', (e) => {
@@ -23,13 +33,17 @@ formulario.addEventListener('submit', (e) => {
         mail.value = ''
         mensaje.value = ''
 
-        obtenerDatos.style.visibility = 'visible'
-})
-
-obtenerDatos.addEventListener('click', (e) => {
-        mensajes[0].innerHTML = "Nombre: " + localStorage.getItem('nombre')
-        mensajes[1].innerHTML = "Mail: " + localStorage.getItem('mail')
-        mensajes[2].innerHTML = "Mensaje: " + localStorage.getItem('mensaje')
+        actualizarDatosMensaje()
 
         datosMensaje.style.visibility = 'visible'
-})      
+        borrarDatos.style.visibility = 'visible'
+})   
+
+borrarDatos.addEventListener('click', (e) => {
+        localStorage.removeItem('nombre')
+        localStorage.removeItem('mail')
+        localStorage.removeItem('mensaje')
+        datosMensaje.style.visibility = 'hidden'
+        borrarDatos.style.visibility = 'hidden'
+
+})
